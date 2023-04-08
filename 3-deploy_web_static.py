b391f397d7cd334235264fc67fc7565a8a03de01
Mshanga Makonde <41159327+mshanga@users.noreply.github.com>
2023-04-08 11:45:56 +0300

    AirBnB_clone_v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,7 +4,7 @@
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['35.237.96.82', '54.164.136.88']
+env.hosts = ['35.174.176.37	', '100.25.160.95']
 
 
 def do_pack():
